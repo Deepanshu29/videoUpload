9f61a6c6d4b26f7dcc4c0018abe5120aa0a6a3cb
Deepanshu Aggarwal <thedeepanshuaggarwal@gmail.com>
2020-08-21 22:27:00 +0530

    first commit

--- src/app/shared/password.validator.ts
+++ src/app/shared/password.validator.ts
@@ -1,7 +1,11 @@
 import { AbstractControl } from '@angular/forms';
 
-export function passwordValidator(control:AbstractControl): {[key: string]: boolean} | null {
-    const password = control.get('password');
-    const cpassword = control.get('cpassword');
-    return password && cpassword && password.value !== cpassword.value ? {misMatch: true} : null;
+export function passwordValidator(
+  control: AbstractControl
+): { [key: string]: boolean } | null {
+  const password = control.get('password');
+  const cpassword = control.get('cpassword');
+  return password && cpassword && password.value !== cpassword.value
+    ? { misMatch: true }
+    : null;
 }
